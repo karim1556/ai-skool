a48def56be1e5cceb88582350c80aca526c033c7
Karim Shaikh <karimshaikh1555@gmail.com>
2025-07-27 20:17:03 +0530

    change to see deployment

--- app/page.tsx
+++ app/page.tsx
@@ -222,7 +222,7 @@ export default function HomePage() {
     },
     {
       id: 5,
-      name: "Educational Board Game Set",
+      name: "Educational Board Game Karim",
       description: "5 STEM-focused board games for classroom use",
       price: 89,
       originalPrice: 120,
